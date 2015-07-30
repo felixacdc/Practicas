d7f78b19ef2ac43a9900a48f4e54ad284c6161c1
Felix Mendez <pfmata01@gmail.com>
2015-07-30 10:46:47 -0600

    [add]
    One

---
+++ Js/Json y php/php/service.php
@@ -0,0 +1,12 @@
+<?php
+	
+	for ($i=0; $i < 10; $i++) { 
+		$datos[] = array("ronda" => $i, "jugadores" => array(
+				array("nombre" => "Suarez", "puntos" => rand(0,100)),
+				array("nombre" => "Messi", "puntos" => rand(0,100)),
+				array("nombre" => "Neymar", "puntos" => rand(0,100))
+			));
+	}
+
+	print_r($datos);
+?>
